939a5a35525f8cebed09c1c3d0c837b20b2518a2
swan0sasha <swan.sasha@gmail.com>
2023-03-28 16:21:47 +0700

    added javadoc

--- src/main/java/MIRoot.java
+++ src/main/java/MIRoot.java
@@ -6,15 +6,27 @@ import java.util.Arrays;
 import java.util.Deque;
 
 public class MIRoot {
-
+    /**
+     * responsible for the order of traversing classes
+     */
     private static Deque<Class<?>> classesQueue;
 
+    /**
+     * Constructor
+     * creates a new classesQueue if it's null
+     */
     public MIRoot() {
         if (classesQueue == null) {
             classesQueue = new ArrayDeque<>();
         }
     }
 
+    /**
+     * @param arguments Arguments of the function for which call-next-method is called
+     * @return the result of the call-next-method
+     * @throws NoSuchMethodException if there is no such method for classes higher in the hierarchy
+     * @throws MIHierarchyException  if there is no parent class to call for next method
+     */
     public Object callNextMethod(Object... arguments) throws NoSuchMethodException, MIHierarchyException {
         String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
         Class<?>[] parents = getParents(this.getClass());
@@ -39,6 +51,9 @@ public class MIRoot {
         throw new NoSuchMethodException("No method \"" + methodName + "(" + Arrays.toString(argumentTypes) + ")\" in hierarchy!");
     }
 
+    /**
+     * @param parents Classes to be added to classesQueue
+     */
     private void addSuperClassesToQueue(Class<?>[] parents) {
         for (Class<?> parent : parents) {
             if (!classesQueue.contains(parent)) {
@@ -47,6 +62,10 @@ public class MIRoot {
         }
     }
 
+    /**
+     * @param c
+     * @return the parents of class c
+     */
     private Class<?>[] getParents(Class<?> c) {
         if (c.getAnnotations().length == 0 ||
                 c.getAnnotation(Extends.class) == null ||
@@ -57,6 +76,10 @@ public class MIRoot {
         return c.getAnnotation(Extends.class).parents();
     }
 
+    /**
+     * @param arguments
+     * @return an array with types for each argument
+     */
     private Class<?>[] getArgumentTypes(Object... arguments) {
         Class<?>[] argumentTypes = new Class[arguments.length];
         for (int i = 0; i < arguments.length; i++) {
@@ -65,6 +88,12 @@ public class MIRoot {
         return argumentTypes;
     }
 
+    /**
+     * @param methodName Method name
+     * @param arguments  Arguments with which this method is called
+     * @return true if the head element of the classesQueue has this method, otherwise false
+     * if the head element of the queue does not have this method, removes it from the queue
+     */
     private boolean hasNextMethod(String methodName, Object... arguments) {
         if (classesQueue.isEmpty()) {
             return false;
@@ -89,6 +118,12 @@ public class MIRoot {
         return true;
     }
 
+    /**
+     * @param methodName Method name
+     * @param arguments  Arguments with which this method is called
+     * @return the result of executing this method
+     * here the head element of the classesQueue is the nearest parent that has this method
+     */
     private Object invokeNextMethod(String methodName, Object... arguments) {
         Class<?>[] argumentTypes = getArgumentTypes(arguments);
         Class<?> parent = classesQueue.pop();
